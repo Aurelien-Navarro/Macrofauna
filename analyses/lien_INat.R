fa527008cae7bc89c53a7ffae83df2a7533f15c0
MikeHedde_desktop <mickael.hedde@inrae.fr>
2023-02-06 14:02:47 +0100

    comparaison liste taxo avant et après envoi de photo sur INat

--- analyses/lien_INat.R
+++ analyses/lien_INat.R
@@ -12,7 +12,7 @@
 
 
 # Libraries
-librarian::shelf(tidyr, dplyr, ggplot2, rinat)
+librarian::shelf(tidyr, dplyr, ggplot2, rinat, rgbif, "inbo/inborutils")
 
 south_lat <- 45.405840
 west_long <- 6.594312
@@ -23,7 +23,22 @@ bounds <- c(south_lat, west_long, north_lat, east_long)
 
 ct <- get_inat_obs(taxon_name = "Animalia", bounds = bounds) %>%   
   filter(user_name == "Mike Hedde") %>%
-  select(scientific_name, description)
+  select(scientific_name, description) %>%
+  rename(INat = description)
 
 ###
 # A continuer: comparer le niveau d'identification gagné sachant qu'il faut ~1h par placette pour faire les photos et les charger sur INat
+
+test <- readxl::read_xlsx("data/raw-data/test_inat_tania.xlsx") 
+
+test2 <- test %>% 
+  left_join(ct) %>%
+  mutate(name = ifelse(is.na(scientific_name), `Valid Name`, scientific_name)) %>%
+  gbif_species_name_match(name = "name") %>%
+  select("id_sample", "Replicate number", "id_plot", "abundance", "stade" , "Valid Name",  "scientificName", "rank", "order", "class", "family", "genus")
+
+ecosols <- length(unique(test2$`Valid Name`))
+inat <- length(unique(test2$scientificName))
+
+(inat-ecosols)/inat*100   # % augmentation du nb de taxon identifié
+
