63a1f26f91af72f01d48bc898c70076665885108
MikeHedde_desktop <mickael.hedde@inrae.fr>
2023-02-13 17:48:08 +0100

    nouvelle requête de données grace aux 'projects' créés sous INat

--- analyses/lien_INat.R
+++ analyses/lien_INat.R
@@ -14,16 +14,22 @@
 # Libraries
 librarian::shelf(tidyr, dplyr, ggplot2, rinat, rgbif, "inbo/inborutils")
 
+
+# En requêtant par le projet INat créé par localité
+ct0 <- get_inat_obs_project(158034, type="observations",raw=T)  # project_id=158034  pour Orchamp_global
+
+# En requêtant par les coordonnées géographiques (WGS84) ici par ex pour La Tania
 south_lat <- 45.405840
 west_long <- 6.594312
 north_lat <- 45.433315
 east_long <- 6.618400 
-
 bounds <- c(south_lat, west_long, north_lat, east_long)
+ct0 <- get_inat_obs(taxon_name = "Animalia", bounds = bounds) 
 
-ct <- get_inat_obs(taxon_name = "Animalia", bounds = bounds) %>%   
-  filter(user_name == "Mike Hedde") %>%
-  select(scientific_name, description) %>%
+# Mise en forme des données
+ct <- ct0 %>%
+  as_tibble() %>%
+  select(taxon.name, taxon.rank, description) %>%
   rename(INat = description)
 
 ###
@@ -33,7 +39,7 @@ test <- readxl::read_xlsx("data/raw-data/test_inat_tania.xlsx")
 
 test2 <- test %>% 
   left_join(ct) %>%
-  mutate(name = ifelse(is.na(scientific_name), `Valid Name`, scientific_name)) %>%
+  mutate(name = ifelse(is.na(taxon.name), `Valid Name`, taxon.name)) %>%
   gbif_species_name_match(name = "name") %>%
   select("id_sample", "Replicate number", "id_plot", "abundance", "stade" , "Valid Name",  "scientificName", "rank", "order", "class", "family", "genus")
 
