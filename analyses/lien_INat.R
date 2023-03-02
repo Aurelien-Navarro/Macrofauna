34611811e1066fc1bc9817fde5dceb33e9a09209
MikeHedde <mickael.hedde@inrae.fr>
2023-03-02 23:31:01 +0100

    mieux, mais il reste des typo sur les noms de certaines espèces qu'il faut modifier dans la bdd

--- analyses/lien_INat.R
+++ analyses/lien_INat.R
@@ -12,12 +12,13 @@
 
 
 # Libraries
-librarian::shelf(tidyr, dplyr, ggplot2, rinat, rgbif, "Rekyt/rtaxref", "inbo/inborutils", RODBC)
+librarian::shelf(tidyr, dplyr, ggplot2, rinat, rgbif, "Rekyt/rtaxref", "inbo/inborutils", "ropensci/rgnparser", RODBC)
 
 # Connection to Mike's Access database
     ## Set up driver info and database path
     DRIVERINFO <- "Driver={Microsoft Access Driver (*.mdb, *.accdb)};"
     MDBPATH <- "C:/Users/Hedde/Nextcloud/Hedde M/1. Travaux/AQR/0. Bases de Données/db communautés/fds_230228.accdb"
+    MDBPATH <- "C:/Users/heddemic/Nextcloud2/Hedde M/1. Travaux/AQR/0. Bases de Données/db communautés/fds_230228.accdb"
     PATH <- paste0(DRIVERINFO, "DBQ=", MDBPATH)
     
     ## Establish connection
@@ -54,19 +55,46 @@ test2 <- df %>%
   left_join(inat_orchamp, by = "INat") %>%
   mutate(name = ifelse(is.na(taxon.name), `Valid Name`, taxon.name)) 
 
-  taxo_harmo <- as.data.frame(matrix(data=NA, nrow = length(names_to_chk), ncol = 8,
-                       dimnames = list(1:length(names_to_chk),
-                                       c("scientificName", "id", "rankName", "referenceName", "familyName", "orderName", "className", "phylumName"))))
-  taxo_harmo$scientificName <- unique(test2$name)
-  
-  taxref_chk <- function(X){
-    chk <- rt_taxa_search(sciname = X) %>%
-      select(id, rankName, referenceName, familyName, orderName, className, phylumName)
-    chk
-  }
-  
-  for(i in 1:nrow(taxo_harmo)){
+# taxref fields to keep
+fields_to_keep <- c("scientificName", "id", "rankName", "referenceName", 
+                    "familyName", "orderName", "className", "phylumName")
+
+# unique taxon names
+uniqueNames_raw <- tibble(name = unique(test2$name)) %>% 
+                          filter(!grepl("Larve", name))
+
+# parse taxon name to clean the list to obtain the most simple canonical name
+uniqueNames <- gn_parse_tidy(uniqueNames_raw$name)$canonicalsimple
+nb_unique <- length(uniqueNames)
+
+# create empty dataframe to store taxref taxonomy
+taxo_harmo <- as.data.frame(matrix(data=NA, 
+                                   nrow = nb_unique, 
+                                   ncol = 8,
+                                   dimnames = list(1:nb_unique, fields_to_keep)))
+taxo_harmo$scientificName <- uniqueNames
+
+# function to retrieve taxonomy from taxref to each canonical name
+taxref_chk <- function(X){
+  chk0 <- rt_taxa_search(sciname = X) 
+  ifelse(ncol(chk0) > 1,
+         chk <- chk0 %>%
+                 arrange(parentId) %>%
+                 filter(row_number()==1) %>%
+                 select(id, rankName, referenceName, familyName, orderName, className, phylumName),
+         chk <- rep(NA, 7))
+  chk
+}
+
+# change Scarabaeoidae"=> "Scarabaeidae
+taxo_harmo <- taxo_harmo %>%
+  mutate(scientificName = ifelse(scientificName == "Scarabaeoidae", "Scarabaeidae", scientificName))
+
+# loop to attribute taxref taxo to all canonical names
+for(i in 1:nrow(taxo_harmo)){
   taxo_harmo[i,2:8] <- taxref_chk(taxo_harmo$scientificName[i])
   }
 
-  
+
+taxref_chk("Auchenorrhyncha")
+rt_taxa_search(sciname = "Aporrectodea giardi giardi")
